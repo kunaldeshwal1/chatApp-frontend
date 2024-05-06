9db15ba7e1e2e21be57d0e602df84e0934a2271b
Kunal <deshwa170@gmail.com>
2024-05-06 21:58:52 +0530

    first

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+  return defineConfig({
+    plugins: [react()],
+    define: {
+      'process.env': { ...process.env, ...loadEnv(mode, process.cwd()) }
+    },
+    server: {
+      host:'0.0.0.0',
+      port: 3000
+    }
+  });
+};
+
+// TODO: https://nowtime.cc/react/1651.html
